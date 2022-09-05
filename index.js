2d478526e6148237de72d99bec287b0cfaef9122
bhav380 <bhaveshrathour380@gmail.com>
2022-09-05 10:13:41 +0530

    setting up ejs

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const port = 8000;
 //setting up router
 app.use('/', require('./routes'));
 
+//setting up ejs
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 
 app.listen(port, function(err) {
     if (err) {
