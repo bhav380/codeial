a95cef6204862f682d7a0d9cc20f4eeb76bfecfb
bhav380 <bhaveshrathour380@gmail.com>
2022-12-12 19:41:11 +0530

    jjj

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,18 @@
+const Post = require('../models/post');
+
+
 module.exports.home = function(req, res) {
     console.log("controller loaded");
 
     console.log(req.cookies);
 
-    res.cookie('name', 'Bhawesh Rathour');
-    return res.render('home', {
-        Title: "Home"
+    Post.find({}, function(err, posts) {
+
+        return res.render('home', {
+            Title: "Home",
+            Posts: posts
+        });
+
     });
+
 }
--- index.js
+++ index.js
@@ -1,44 +1,79 @@
-const express = require("express");
-
+const express = require('express');
 const cookieParser = require('cookie-parser');
-
 const app = express();
-
 const port = 8000;
-
-const db = require('./config/mongoose');
-
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-stratergy');
 
-app.use(expressLayouts);
+const MongoStore = require('connect-mongo');
 
-app.use(express.static('./assets'));
+const sassMiddleware = require('node-sass-middleware');
 
-const bodyParser = require('body-parser');
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(express.urlencoded());
 
 app.use(cookieParser());
 
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-//setting up router
-app.use('/', require('./routes'));
 
-//setting up ejs
 
+
+// set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 
+app.use(session({
+    name: 'codeial',
+    // TODO change the secret before deployment in production mode
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    },
+
+    store: MongoStore.create({
+            mongoUrl: 'mongodb://localhost:27017/mongo-codeial',
+            autoRemove: 'disabled'
+
+        },
+        function(err) {
+            console.log(err || 'connect-Mongdb store ok');
+            return;
+        }
+    )
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.setAuthenticatedUser);
+
+// use express router
+app.use('/', require('./routes'));
+
+
 app.listen(port, function(err) {
     if (err) {
-        console.log("ERror in setting server");
-        return;
+        console.log(`Error jj in running the server: ${err}`);
     }
 
-    console.log("Server is running at port number : ", port);
-    return;
+    console.log(`Server is running on port: ${port}`);
 });
